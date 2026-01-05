122451b86c6eec93d6d264de82e478bda5bf0c03
Edson-Emanoel <edson.emanoelofc@gmail.com>
2026-01-05 18:09:27 -0300

    feat: terminei a nona aula

---
+++ aula-09.js
@@ -0,0 +1,20 @@
+/** 
+ * Iniciando If else
+*/
+
+nome = "Pedro"
+idade = 15
+sexo = "m"
+
+if(sexo == "f") {
+    sexo == "feminino"
+    console.log(sexo);
+    return;
+    
+} else {
+    sexo == "masculino"
+    console.log(sexo);
+    return;
+}
+
+console.log(sexo);
