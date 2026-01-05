2d17619b586a1756f22fc3b56fa12ac44399ad97
Edson-Emanoel <edson.emanoelofc@gmail.com>
2026-01-05 17:59:36 -0300

    feat: finalizei a aula-08

---
+++ aula-08.js
@@ -0,0 +1,13 @@
+/** 
+ * Iniciando If else
+*/
+
+nome = "ana"
+idade = 25
+
+if(idade < 18) {
+    console.log(nome + "não pode sair à noite!");
+    return;
+}
+
+console.log(nome + "pode sair à noite");
